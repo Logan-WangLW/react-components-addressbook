3ac9f632030ac1a754b3d1e53aff7b7e4306347d
Logan Wang <loganxwang@hotmail.com>
2018-11-05 11:18:09 -0800

    completed drill

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,5 +1,15 @@
+/* eslint-disable */
 import React from 'react';
 
 import './contact.css';
 
 // The Contact component goes here.  It should be the default export.
+export default function Contact(props) {
+  return (
+    <div className="contact">
+      <p className="contact-name">{props.name}</p>
+      <p className="contact-address">{props.address}</p>
+      <img className="contact-photo" src={props.photo}></img>
+    </div>
+  )
+}
